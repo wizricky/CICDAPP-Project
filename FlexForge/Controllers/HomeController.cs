bd4803569dce8b68e827a69fba38049898a0b138
mmate <mmatej.minoski@gmail.com>
2024-08-12 18:18:03 +0200

    Designing the home page and about page

--- FlexForge/Controllers/HomeController.cs
+++ FlexForge/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Eshop.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
